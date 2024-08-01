754fc3d2b49c227f5514b6dc78b8704d309c4a2f
Topi L <taolaitinen@gmail.com>
2024-08-01 18:52:52 +0300

    Just a test with ChatGPT and VSCode. Trying to make a window :)

---
+++ main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Full Screen App"),
+      ),
+      body: Center(
+        child: Text("Hello, World!"),
+      ),
+    );
+  }
+}
